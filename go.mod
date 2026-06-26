9b82596a1df8dd0a6c3f8f379f2428befbe1b56b
harsh-zanskar <harsh@zanskarsec.com>
2026-06-26 15:20:56 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/test-repo
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("this is test repo")
+}
+
+// func foo() {
+// 	fmt.Printf("prints foo")
+// }
